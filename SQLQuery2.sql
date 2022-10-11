b1c60944d011401680b2789f6e5d2466e53aa21d
AbhiShinde1 <abhishinde572@gmail.com>
2022-10-11 17:23:52 +0530

    AddressBook Table created

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -3,3 +3,17 @@ create database AddressBook
 
 use  AddressBook
 
+----UC2 Creating Table------
+select * from  Address_Book
+create table Address_Book
+(
+id int identity (1,1) primary key,
+firstname varchar (250),
+lastname varchar (250),
+Address varchar (200),
+city varchar (200),
+state varchar (200),
+zip int,
+PhoneNumber bigint,
+email varchar (250)
+);
